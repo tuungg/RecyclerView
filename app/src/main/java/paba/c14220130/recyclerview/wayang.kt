ea4fea2946503d3358aeda75a5ced7f4a6bebc88
tuungg <theo.thung1204@gmail.com>
2024-11-05 20:39:09 +0700

    Changes

---
+++ app/src/main/java/paba/c14220130/recyclerview/wayang.kt
@@ -0,0 +1,16 @@
+package paba.c14220130.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+data class wayang(
+    var foto : String,
+    var nama : String,
+    var karakter : String,
+    var deskripsi : String
+)
+
+
